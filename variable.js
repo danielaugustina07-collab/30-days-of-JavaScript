5f82d3fa291e0f4deb33192b914b33e51c3100a5
Augustina <danielaugustina07@gmail.com>
2026-07-15 20:12:46 +0100

    Add initial files for 30 Days of JavaScript project

---
+++ variable.js
@@ -0,0 +1,20 @@
+let firstName;
+let lastName;
+let country;
+let age;
+
+
+let city = "Lagos";
+let school = "SLT";
+let score = 80;
+let passed = true;
+
+
+let firstName = "Tina";
+let lastName = "Daniel";
+let maritalStatus = "Single";
+let country = "Nigeria";
+let age = 25;
+
+
+let firstName = "Tina", lastName = "Daniel", maritalStatus = "Single", country = "Nigeria", age = 25;
